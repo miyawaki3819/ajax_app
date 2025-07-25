9daf9ea8830eda5c7602f687e876ac0c17e49a56
miyawaki3819 <soler.creus@gmail.com>
2025-07-25 09:52:03 +0900

    回答

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,4 +1,9 @@
-// Ajaxによる処理が終了したあとに呼び出されるイベント
+// レスポンスのうち、コントローラー側で指定したjson形式のデータを変数に代入
+// 今回投稿したデータを追加する要素を取得
+// フォーム投稿の際にテキストを入力した、テキストエリアを取得
+// 追加する要素を定義
+// 親要素に直前で定義した要素を追加
+// フォームの入力欄を空にする
 function post (){
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
@@ -8,7 +13,15 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      console.log(XHR.response.article);
+      const item = XHR.response.article;
+      const contentsArea = document.getElementById("contents_area");
+      const articleText = document.getElementById("article_text");
+      const HTML = `
+        <div class="article">
+          ${ item.text }
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      articleText.value = "";
     };
     e.preventDefault();
   });
